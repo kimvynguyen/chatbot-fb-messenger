8c9b1f7e45076fce961f00d98b06e28cfc362efd
kimvynguyen <49752136+kimvynguyen@users.noreply.github.com>
2019-09-18 17:11:16 +0700

    Update app.py

--- app.py
+++ app.py
@@ -71,13 +71,13 @@ def webhook():
                     sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
                     message_text = messaging_event["message"]["text"]  # the message's text
-                    send_message(sender_id,message_text)
+                    send_message(sender_id,"chao ban")
 
             if messaging_event.get("postback"):  # user clicked/tapped "postback" button in earlier message
                 sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
                 recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
                 message_text = messaging_event["message"]["text"]  # the message's text
-                if message_text=="Chia se":
+                if message_text=="Mua hang online":
                     webview(sender_id)
                 if message_text=="Chia se":
                     share(sender_id)
