b5451f9f3a1a55a21154ad438d0af00ffeb808b2
kimvynguyen <49752136+kimvynguyen@users.noreply.github.com>
2019-09-20 08:48:10 +0700

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,41 @@ def verify():
 @app.route('/', methods=['POST'])
 
 def webhook():
-   
+   params = {
+        "access_token": "EAAFvTbGl9ccBAMu6fvHRktSf2x8F1blQurZApgpLPZAzou2zjjQGszOao81pAfkqgMlJUKxt7K7OFSdj5gDdZAS59hjXKpyM7egY0NZArNnToM1FZCUHNVpjGxmuHzR4ZAZBpCckXWRq6HMxfRxzOStGdesEOw06Yl4hrKjyH4Kvv3Ip9DDeVglMAkBZBUgZCFC0ZD"
+    }
+    headers = {
+        "Content-Type": "application/json"
+    }
+    data =json.dumps({"get_started":{
+            "payload":"GET_STARTED_PAYLOAD"},
+            "greeting":[
+            {
+                "locale":"default",
+                "text":"Xin chao {{user_full_name}}, cam on ban da quan tam den chung toi! Hay nhan Bat dau de tuong tac cung chung toi nhe!"
+            }],
+            "persistent_menu":[
+            {
+            "locale":"default",
+            "composer_input_disabled": True,
+            "call_to_actions":[
+                {
+                    "type": "web_url",
+                    "title": "Power by IChat",
+                    "url": "https://ichat.vmarketing.vn/",
+                    "webview_height_ratio": "full"
+                },
+                {
+                    "type":"postback",
+                    "title":"Chat voi nhan vien",
+                    "payload": "CARE_HELP"
+                }
+            ]
+            }
+        ],
+        "whitelisted_domains":["https://www.mykingdom.com.vn",
+                              "https://vmarketing.vn"],
+        })
     # endpoint for processing incoming messaging events
     data = request.get_json()
     log(data)  # you may not want tolog every incoming message in production, but it's good for testing
