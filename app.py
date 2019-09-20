6207fd75db43948e2b12557715f97397352a46ff
kimvynguyen <49752136+kimvynguyen@users.noreply.github.com>
2019-09-20 15:43:42 +0700

    Update app.py

--- app.py
+++ app.py
@@ -40,8 +40,8 @@ def webhook():
                     sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
                     if messaging_event['postback']['payload'] == "{\"type\":\"legacy_reply_to_message_action\",\"message\":\"Get Started\"}":
-                        send_attachment(sender_id,"ichat")
                         send_message(sender_id, "Cam on ban da chon Ichat la noi tin tuong lam nen tang.")
+                        send_attachment(sender_id,"ichat")
                     elif messaging_event['postback']['payload'] == "Payload Mua hang online":
                         webview(sender_id,"Mua hang online")
                     elif messaging_event['postback']['payload'] == "Payload Chia se":
