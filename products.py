c95861fe7e2e4a7ea9c17c33577f5016c95a5fbd
kimvynguyen <49752136+kimvynguyen@users.noreply.github.com>
2019-09-21 09:31:51 +0700

    Update products.py

--- products.py
+++ products.py
@@ -11,50 +11,69 @@ def list_template(recipient_id, message_text):
     "recipient": {
             "id": recipient_id
         },
-    "message":{
-        "attachment": {
-        "type": "template",
-        "payload": {
-            "template_type": "generic",
-            "elements": [
-            [{
-                "title": "Do choi van dong",
-                "subtitle": "Xem danh sach do choi van dong",
-                "image_url": "https://www.mykingdom.com.vn/media/catalog/product/cache/73a368d029059537f29915590ba445d6/magento/ROYAL_BABY/RB16B-6___RED/RB16B-6___RED_1.jpg",          
-                "buttons": [
-                {
-                    "type": "postback",
-                    "title": "Xem danh sach",
-                    "payload": "{\"type\":\"legacy_reply_to_message_action\",\"message\":\"Do choi van dong\"}"
-                },
-                ],
-            }],
-            [{
-                "title": "Do choi tri tue",
-                "subtitle": "Xem danh sach do choi tri tue",
-                "image_url": "https://www.mykingdom.com.vn/media/catalog/product/cache/73a368d029059537f29915590ba445d6/magento/MAISTO/MT39194/MT39194_1.jpg",
-                "buttons": [
-                {
-                    "type": "postback",
-                    "title": "Xem danh sach",
-                    "payload": "Payload DCTT",   
-                },
-                ],
-            }],
-            [{
-                "title": "Phuong tien giao thong",
-                "subtitle": "Xem danh sach phuong tien giao thong",
-                "image_url": "https://www.mykingdom.com.vn/media/catalog/product/cache/73a368d029059537f29915590ba445d6/magento/SKY_ROVER/YW859110-6/YW859110-6_1.jpg",
-                "buttons": [
-                {
-                    "type": "postback",
-                    "title": "Xem danh sach",
-                    "payload": "Payload PTGT",             
-                },
-                ],        
-            }]
+    "message": {
+    "attachment": {
+      "type": "template",
+      "payload": {
+        "template_type": "list",
+        "top_element_style": "compact",
+        "elements": [
+          {
+            "title": "Classic T-Shirt Collection",
+            "subtitle": "See all our colors",
+            "image_url": "https://peterssendreceiveapp.ngrok.io/img/collection.png",          
+            "buttons": [
+              {
+                "title": "View",
+                "type": "web_url",
+                "url": "https://peterssendreceiveapp.ngrok.io/collection",
+                "messenger_extensions": True,
+                "webview_height_ratio": "tall",
+                "fallback_url": "https://peterssendreceiveapp.ngrok.io/"            
+              }
             ]
-        }
+          },
+          {
+            "title": "Classic White T-Shirt",
+            "subtitle": "See all our colors",
+            "default_action": {
+              "type": "web_url",
+              "url": "https://peterssendreceiveapp.ngrok.io/view?item=100",
+              "messenger_extensions": False,
+              "webview_height_ratio": "tall"
+            }
+          },
+          {
+            "title": "Classic Blue T-Shirt",
+            "image_url": "https://peterssendreceiveapp.ngrok.io/img/blue-t-shirt.png",
+            "subtitle": "100% Cotton, 200% Comfortable",
+            "default_action": {
+              "type": "web_url",
+              "url": "https://peterssendreceiveapp.ngrok.io/view?item=101",
+              "messenger_extensions": True,
+              "webview_height_ratio": "tall",
+              "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
+            },
+            "buttons": [
+              {
+                "title": "Shop Now",
+                "type": "web_url",
+                "url": "https://peterssendreceiveapp.ngrok.io/shop?item=101",
+                "messenger_extensions": True,
+                "webview_height_ratio": "tall",
+                "fallback_url": "https://peterssendreceiveapp.ngrok.io/"            
+              }
+            ]        
+          }
+        ],
+         "buttons": [
+          {
+            "title": "View More",
+            "type": "postback",
+            "payload": "payload"            
+          }
+        ]  
+      }
         }
     }
         })
